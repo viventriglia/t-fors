b35b6c3199df86fcdc8f91def65e2ef71f85b265
Vincenzo <vincenzo.ventriglia@outlook.com>
2023-12-18 17:13:07 +0100

    added SHAP values, optimisation in WIP state

--- src/forecast.py
+++ src/forecast.py
@@ -1,7 +1,9 @@
 from typing import Literal
 
 import pandas as pd
-from catboost import CatBoostClassifier
+from catboost import CatBoostClassifier, Pool
+from sklearn.metrics import f1_score
+import optuna
 
 
 def instantiate_and_fit_model(
@@ -38,3 +40,33 @@ def instantiate_and_fit_model(
     model = CatBoostClassifier(loss_function=loss_function, **params)
     model.fit(X_train, y_train, eval_set=(X_test, y_test), silent=True)
     return model
+
+
+def objective(
+    trial: optuna.Trial,
+    X_train: pd.DataFrame,
+    y_train: pd.DataFrame,
+    X_test: pd.DataFrame,
+    y_test: pd.DataFrame,
+    params: dict,
+) -> float:
+    params.update(
+        {
+            "iterations": trial.suggest_int("iterations", 100, 2000),
+            "learning_rate": trial.suggest_float("learning_rate", 0.001, 0.1),
+            "depth": trial.suggest_int("depth", 4, 10),
+            "od_wait": trial.suggest_int("od_wait", 100, 300),
+        }
+    )
+
+    model = instantiate_and_fit_model(
+        X_train=X_train,
+        y_train=y_train,
+        X_test=X_test,
+        y_test=y_test,
+        loss_function="Logloss",
+        params=params,
+    )
+
+    y_pred = model.predict(X_test)
+    return f1_score(y_test, y_pred)
