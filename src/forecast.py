a6699896313bdbbf272d48c5f870647753233b90
Vincenzo <vincenzo.ventriglia@outlook.com>
2023-12-19 15:40:09 +0100

    implemented objective (cross-validated) function for hyperparam optimisation

--- src/forecast.py
+++ src/forecast.py
@@ -1,8 +1,10 @@
 from typing import Literal
 
 import pandas as pd
-from catboost import CatBoostClassifier, Pool
+import numpy as np
+from catboost import CatBoostClassifier
 from sklearn.metrics import f1_score
+from sklearn.model_selection import TimeSeriesSplit
 import optuna
 
 
@@ -44,29 +46,59 @@ def instantiate_and_fit_model(
 
 def objective(
     trial: optuna.Trial,
-    X_train: pd.DataFrame,
-    y_train: pd.DataFrame,
-    X_test: pd.DataFrame,
-    y_test: pd.DataFrame,
+    X: pd.DataFrame,
+    y: pd.Series,
+    cv: TimeSeriesSplit,
     params: dict,
 ) -> float:
-    params.update(
-        {
-            "iterations": trial.suggest_int("iterations", 100, 2000),
-            "learning_rate": trial.suggest_float("learning_rate", 0.001, 0.1),
-            "depth": trial.suggest_int("depth", 4, 10),
-            "od_wait": trial.suggest_int("od_wait", 100, 300),
-        }
-    )
+    """
+    Convenience function performing Bayesian optimisation on a set of model hyper-parameters
 
-    model = instantiate_and_fit_model(
-        X_train=X_train,
-        y_train=y_train,
-        X_test=X_test,
-        y_test=y_test,
-        loss_function="Logloss",
-        params=params,
-    )
+    Parameters
+    ----------
+    trial : optuna.Trial
+        Trial object, namely a process of evaluating an objective function
+    X : pd.DataFrame
+        Full features DataFrame, to be split according to the cv object
+    y : pd.Series
+        Full target Series, to be split according to the cv object
+    cv : TimeSeriesSplit
+        Time Series cross-validator object
+    params : dict
+        "Static" hyper-parameters of the model, not subject to optimisation
 
-    y_pred = model.predict(X_test)
-    return f1_score(y_test, y_pred)
+    Returns
+    -------
+    float
+        Average F1 score after cross validation
+    """
+    # Hyper-parameters subject to optimisation
+    params_to_be_optimised = {
+        "iterations": trial.suggest_int("iterations", 100, 5_000),
+        "learning_rate": trial.suggest_float("learning_rate", 0.0001, 0.1),
+        "depth": trial.suggest_int("depth", 2, 10),
+        "od_wait": trial.suggest_int("od_wait", 100, 500),
+        "min_data_in_leaf": trial.suggest_int("min_data_in_leaf", 1, 100),
+    }
+    params.update(params_to_be_optimised)
+
+    f1_scores = []
+
+    # Cross-validated training
+    for train_idx, test_idx in cv.split(X):
+        X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+        y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
+
+        model = instantiate_and_fit_model(
+            X_train=X_train,
+            y_train=y_train,
+            X_test=X_test,
+            y_test=y_test,
+            loss_function="Logloss",
+            params=params,
+        )
+
+        y_pred = model.predict(X_test)
+        f1_scores.append(f1_score(y_test, y_pred))
+
+    return np.mean(f1_scores)
