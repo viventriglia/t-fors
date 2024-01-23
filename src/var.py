d8c98531981bccb6fdd8d4c140d2c67cb0a1dca1
Vincenzo <vincenzo.ventriglia@outlook.com>
2024-01-23 16:46:56 +0100

    data path variable

--- src/var.py
+++ src/var.py
@@ -1,7 +1,12 @@
+from pathlib import Path
+
 # Solar Zenith Angle
 LATITUDE = 50.110656
 LONGITUDE = 8.682526
 ALTITUDE = 350_000
+
+# Data
+DATA_IN = Path("..", "data", "in")
 
 # MLFlow
 ML_SERVER_URI = "http://localhost:5000"
