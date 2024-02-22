b9364ea767a514e6ae74759df5e00249689319ea
Vincenzo <vincenzo.ventriglia@outlook.com>
2024-02-22 16:32:42 +0100

    added first chart (features vs target)

--- app/pages/2_🔮_ML_model.py
+++ app/pages/2_🔮_ML_model.py
@@ -16,7 +16,20 @@ st.set_page_config(
 st.markdown(GLOBAL_STREAMLIT_STYLE, unsafe_allow_html=True)
 
 df_eval = pd.read_pickle(Path(DATA_PATH, "df_eval.pickle"))
-time_period = "2022-02"
 
-fig = plot_features_and_target(df=df_eval, time_period=time_period)
+st.markdown("## ML model evaluation")
+
+start_date, end_date = (
+    df_eval.loc["2022-03"].index.min(),
+    df_eval.loc["2022-04-15"].index.max(),
+)
+
+first_day, last_day = st.select_slider(
+    "Chose the period to display",
+    options=df_eval.index,
+    value=[start_date, end_date],
+    format_func=lambda value: str(value)[:10],
+)
+
+fig = plot_features_and_target(df=df_eval, time_period=[first_day, last_day])
 st.plotly_chart(fig, use_container_width=True, config=PLT_CONFIG_NO_LOGO)
--- app/plot.py
+++ app/plot.py
@@ -4,22 +4,31 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-from var import DATA_PATH, PLT_FONT_SIZE
+from var import PLT_FONT_SIZE
 
 
 def plot_features_and_target(
-    df: pd.DataFrame, features: list = None, time_period: str = None
+    df: pd.DataFrame,
+    time_period: list,
 ) -> go.Figure:
-    df_plt = df.loc[f"{time_period}", ["hf", "iu_mav_3h", "smr", "true", "pred"]]
+    features_dict = {
+        "hf": "HF-EU index",
+        "iu_mav_3h": "Auroral electrojet (IU index, 3h moving average)",
+        "smr": "SMR (ring current)",
+        "true": "Ground truth (from catalogue)",
+        "pred": "Model prediction",
+    }
 
-    n_cols = len(df_plt.columns)
+    df_plt = df.loc[f"{time_period[0]}":f"{time_period[-1]}", features_dict.keys()]
+
+    n_cols = len(features_dict.keys())
 
     fig = make_subplots(
         rows=n_cols,
         cols=1,
         shared_xaxes=True,
         vertical_spacing=0.04,
-        subplot_titles=df_plt.columns,
+        subplot_titles=[*features_dict.values()],
     )
 
     for i, col in enumerate(df_plt.columns, start=1):
@@ -27,17 +36,23 @@ def plot_features_and_target(
             go.Scatter(
                 x=df_plt[col].index,
                 y=df_plt[col].values,
-                name=col,
+                name=features_dict[col],
+                showlegend=False,
             ),
             row=i,
             col=1,
         )
+        fig.update_traces(hovertemplate="<b>%{y:,.1f}</b> <extra>%{x}</extra>")
 
     fig.update_layout(
+        margin=dict(b=0),
         template="plotly_white",
         height=800,
         width=1_000,
-        autosize=False,
-        title=f"Period: <b>{time_period}</b>",
+        # hoverlabel_font_size=PLT_FONT_SIZE,
+        title=dict(
+            text=f"<b>Features, target and model prediction, from {time_period[0].date()} to {time_period[-1].date()}</b>",
+            font_size=PLT_FONT_SIZE + 4,
+        ),
     )
     return fig
