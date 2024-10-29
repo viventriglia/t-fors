ce8c494497b9336492d7e9c019e150e227144ac3
Vincenzo <vincenzo.ventriglia@outlook.com>
2024-10-29 18:58:03 +0100

    variables for openapi swagger infos

--- src/__init__.py
+++ src/__init__.py
@@ -11,3 +11,28 @@ DATA_IN = Path("..", "data", "in")
 # MLFlow
 ML_SERVER_URI = "http://localhost:5000"
 EXPERIMENT_NAME = "CatBoost"
+
+# FastAPI
+FASTAPI_SUMMARY = """Traveling Ionospheric Disturbances Forecasting System
+(T-FORS), funded by the European Community, Horizon Europe
+"""
+FASTAPI_DESC = """ChimichangApp API helps you do awesome stuff. 🚀
+
+## Items
+
+You can **read items**.
+
+## Users
+
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_)."""
+FASTAPI_CONTACT = {
+    "name": "The T-FORS Project",
+    "url": "https://t-fors.eu/",
+}
+FASTAPI_LICENSE = {
+    "name": "MIT License",
+    "url": "https://opensource.org/license/mit",
+}
